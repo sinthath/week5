20e17a93c9f45215763f0fc3b91fb67edf4e7008
sinthath <sinthathirath@gmail.com>
2021-12-19 21:52:00 -0500

    time block status

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,30 @@ var mnt = moment().format('MMM Do YYYY');
 var showtime = $('#currentDay');
 showtime.text(mnt);
 console.log(mnt);
+
+// checks each time block to determine time lapse and apply applicable styling to element id
+function timeTrack () {
+    var time = moment().hours();
+
+    $(".input").each(function() {
+        var timeSelect = parseInt($(this).attr("id"));
+
+        if (time > timeSelect) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }else if (time < timeSelect) {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }else {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+    });
+}
+
+$(document).ready(function() {
+    timeTrack();
+});
